06fc8fc291e176d2dc95b0b0f11dd012e14c2b31
helena <helenawang77@gmail.com>
2020-05-14 22:28:16 -0700

    create HomeControllerTest.privacy test

--- MovieMvcTest/HomeControllerTest.cs
+++ MovieMvcTest/HomeControllerTest.cs
@@ -23,5 +23,19 @@ namespace MovieMvcTest
             Assert.IsType<ViewResult>(result);
 
         }
+
+        [Fact]
+        public void Privacy_ActionExecutes_ReturnsAViewResult()
+        {
+            // Arrange
+            var homeController = new HomeController(_logger);
+
+            // Act
+            var result = homeController.Privacy();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+
+        }
     }
 }
