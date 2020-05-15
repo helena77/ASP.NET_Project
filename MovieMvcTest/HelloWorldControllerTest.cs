537c18d14de9d919c578655af1f2607dc798e431
helena <helenawang77@gmail.com>
2020-05-14 21:35:07 -0700

    create hellowordcontrollertest.index test

---
+++ MovieMvcTest/HelloWorldControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Controllers;
+using Xunit;
+
+namespace MovieMvcTest
+{
+    public class HelloWorldControllerTest
+    {
+        [Fact]
+        public void Index_ActionExecutes_ReturnsAViewResult()
+        {
+            // Arrange
+            var helloWorldController = new HelloWorldController();
+
+            // Act
+            var result = helloWorldController.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+
+        }
+    }
+}
