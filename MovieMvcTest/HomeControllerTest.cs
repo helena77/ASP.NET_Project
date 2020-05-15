bb7df45151baa0ecc42ae6e29b906fe474b983c9
helena <helenawang77@gmail.com>
2020-05-14 22:26:14 -0700

    create HomeControllerTest.index test

---
+++ MovieMvcTest/HomeControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieMvc.Controllers;
+using Xunit;
+
+namespace MovieMvcTest
+{
+    public class HomeControllerTest
+    {
+        ILogger<HomeController> _logger;
+
+        [Fact]
+        public void Index_ActionExecutes_ReturnsAViewResult()
+        {
+            // Arrange
+            var homeController = new HomeController(_logger);
+
+            // Act
+            var result = homeController.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+
+        }
+    }
+}
