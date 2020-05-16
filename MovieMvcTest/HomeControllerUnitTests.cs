c2057c4aa5e90aa6ef14f4bf40888f71eeecc982
helena <helenawang77@gmail.com>
2020-05-16 15:26:18 -0700

    update unittest

---
+++ MovieMvcTest/HomeControllerUnitTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
+using Moq;
+using MovieMvc.Controllers;
+using MovieMvc.Models;
+using Xunit;
+
+namespace MovieMvcTest
+{
+    public class HomeControllerUnitTests
+    {
+        ILogger<HomeController> _logger;
+
+        [Fact]
+        public void Index_ActionExecutes_ReturnsAViewResult()
+        {
+            // Arrange
+            var homeController = new HomeController(_logger);
+
+            // Act
+            var result = homeController.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+
+        }
+
+        [Fact]
+        public void Privacy_ActionExecutes_ReturnsAViewResult()
+        {
+            // Arrange
+            var homeController = new HomeController(_logger);
+
+            // Act
+            var result = homeController.Privacy();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+
+        }
+    }
+}
---
+++ MovieMvcTest/MoviesControllerUnitTests.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using MovieMvc.Controllers;
+using MvcMovie.Data;
+using MvcMovie.Models;
+using Xunit;
+
+namespace MovieMvcTest
+{
+    public class MoviesControllerUnitTests
+    {
+     
+       
+    }
+}
