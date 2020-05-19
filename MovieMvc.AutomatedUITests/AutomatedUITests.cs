66f527f62089c46b6724e45378d5a32542d5f2a7
helena <helenawang77@gmail.com>
2020-05-19 13:07:26 -0700

    update automateduitest

---
+++ MovieMvc.AutomatedUITests/AutomatedUITests.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace MovieMvc.AutomatedUITests
+{
+    public class AutomatedUITests : IDisposable
+    {
+        private readonly IWebDriver _driver;
+        public AutomatedUITests()
+        {
+            _driver = new ChromeDriver(Directory.GetCurrentDirectory());
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+
+        [Fact]
+        public void Index_WhenExecuted_ReturnsBingView()
+        {
+            _driver.Navigate().GoToUrl("https://www.bing.com/");
+
+            Assert.Equal("Bing", _driver.Title);
+        }
+    }
+}
